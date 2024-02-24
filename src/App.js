4a5cb6ab2909b15510068ce5dfcac52c451f8257
Kevin Lee <hikevinmlee@gmail.com>
2024-02-24 23:14:13 +0000

    refactor to use react

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+  const [UDPInPort, setUDPInPort] = useState(9999);
+  const [socketPort, setSocketPort] = useState(8081);
+  const [UDPOutPort, setUDPOutPort] = useState(9998);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    electronAPI.onOSCMsg((msg) => {
+      setMessages([...messages, msg]);
+    });
+  }, [electronAPI]);
+
+  return (
+    <>
+      <div className="row">
+        <label htmlFor="udpinport">Receiving OSC UDP Port</label>
+        <div>
+          <input
+            type="number"
+            id="udpinport"
+            name="udpinport"
+            required
+            size="10"
+            value={UDPInPort}
+            onChange={(e) => setUDPInPort(e.target.value)}
+            onBlur={(e) => electronAPI.setUDPPort(e.target.value)}
+          />
+          <button
+            id="udpinportset"
+            onClick={() => electronAPI.setUDPPort(UDPInPort)}
+          >
+            Set
+          </button>
+        </div>
+      </div>
+
+      <div className="row">↓</div>
+
+      <div className="row">
+        <label htmlFor="socketport">OSC Websocket Port</label>
+        <div>
+          <input
+            type="number"
+            id="socketport"
+            name="socketport"
+            required
+            size="10"
+            value={socketPort}
+            onChange={(e) => setSocketPort(e.target.value)}
+            onBlur={(e) => electronAPI.setSocketPort(e.target.value)}
+          />
+          <button
+            id="socketportset"
+            onClick={() => electronAPI.setSocketPort(socketPort)}
+          >
+            Set
+          </button>
+        </div>
+      </div>
+
+      <br />
+
+      <div className="row">
+        <label htmlFor="socketoutport">OSC Websocket Port</label>
+        <div>
+          <input
+            type="number"
+            id="socketoutport"
+            name="socketoutport"
+            required
+            size="10"
+            value={socketPort}
+            disabled
+          />
+          <button id="socketoutportset" disabled>
+            Set
+          </button>
+        </div>
+      </div>
+
+      <div className="row">↓</div>
+
+      <div className="row">
+        <label htmlFor="udpoutport">Sending OSC UDP Port</label>
+        <div>
+          <input
+            type="number"
+            id="udpoutport"
+            name="udpoutport"
+            required
+            value={UDPOutPort}
+            onChange={(e) => setUDPOutPort(e.target.value)}
+            onBlur={(e) => electronAPI.setUDPOutPort(e.target.value)}
+          />
+          <button
+            id="udpoutportset"
+            onClick={() => electronAPI.setUDPOutPort(UDPOutPort)}
+          >
+            Set
+          </button>
+        </div>
+      </div>
+
+      <ul id="messages">
+        {messages.map(({type, msg}) => (
+          <>
+            <div className={`badge ${type}`}>{type}</div>
+            <code>{msg}</code>
+          </>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-const {app, BrowserWindow, ipcMain} = require('electron/main');
-const path = require('node:path');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
 
 app.setName('OSC Relay');
 
@@ -38,15 +43,17 @@ const wss = new WebSocket.Server({
 // -------------------------- END OSC --------------------------
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 400,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
   });
 
-  win.loadFile('index.html');
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Set up OSC message forwarding to the frontend
   udpPort.on('message', function (msg) {
@@ -72,7 +79,10 @@ const createWindow = () => {
   });
 };
 
-app.whenReady().then(() => {
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
   ipcMain.on('set-udp-port', (event, arg) => {
     console.log('Changing UDP in port to', arg);
     udpPort.options.localPort = arg;
@@ -88,13 +98,26 @@ app.whenReady().then(() => {
     udpPort.options.remotePort = arg;
     udpPort.open();
   });
+
   createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 });
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
